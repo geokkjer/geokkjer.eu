e21a7d42055d95ce423a79c2d7458374a1915e8e
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-01-11 13:25:39 +0000

    many changes, might move this repo to sr.ht

--- build-site.el
+++ build-site.el
@@ -28,14 +28,28 @@
 	     :with-creator t
 	     :with-toc nil
 	     :section-numbers nil
-	     :time-stamp-file nil)))
+	     :time-stamp-file nil
+	     :html-doctype "html5"
+	     :html-html5-fancy t)))
 
 ;; customize thte html output
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
-      ;; org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">")
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>"
+      org-html-preamble "<nav>
+    <div class=\"left\">
+      <a href=\"/index.html\">Home</a> |
+      <a href=\"/systems/index.html\">Systems</a> |
+      <a href=\"/articles/index.html\">Articles</a> |
+      <a href=\"/about.html\">About</a> |
+      <a href=\"/cv.html\">CV</a> |
+      <a href=\"/emacs/index.html\">Emacs</a> |
+      <a href=\"/emacs/index.html\">Sitemap</a> |
+      <a href=\"/nixos/index.html\">NixOS</a>
+    </div>
+    </nav>")
+      ;; org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">")
 
 ;; Generate the site output
 (org-publish-all t)
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 emacs -Q --script build-site.el
+
+# copy public to www folder
+sudo cp -rv /home/geir/geokkjer.eu/public /data/www/
